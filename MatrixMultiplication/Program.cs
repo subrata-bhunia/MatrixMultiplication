4addf019baa7901f6e8dab05f847ee5f396ee73c
subrata-bhunia <eerrcfgy@gmail.com>
2022-12-04 13:14:06 +0530

    first commit

---
+++ MatrixMultiplication/Program.cs
@@ -0,0 +1,82 @@
+﻿// Ex : 5.1 (ii)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, m, n;
+            Console.WriteLine("Enter the Number of Rows and Columns : ");
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            int[,] b = new int[m, n];
+            int[,] c = new int[m, n];
+            Console.WriteLine("Enter the First Matrix");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            
+            Console.WriteLine("Enter the Second Matrix");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    b[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("First matrix is:");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("Second Matrix is :");
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write(b[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Matrix Multiplication is :");
+            
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < 2; k++)
+                    {
+                        c[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write(c[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
